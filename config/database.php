9cd6b1f034a20bdfc10f31d44d2995dd7a686125
Christ Nakhoul <christnakh@gmail.com>
2025-07-28 00:21:48 +0300

    removed unused files

--- config/database.php
+++ config/database.php
@@ -4,7 +4,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Database configuration for MAMP
 $host = 'localhost:8889';
 $dbname = 'lecon_particuliere';
 $username = 'root';
